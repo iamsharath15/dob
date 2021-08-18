577db78690596fdb34ea96f44ce85e1bf563769d
iamsharath15 <iamsharath15@gmail.com>
2021-08-18 22:11:03 +0530

    new

---
+++ index.js
@@ -0,0 +1,17 @@
+function ageInDays(){
+    var bob =prompt("dob?");
+    var day = (2021 - bob) * 365;
+    var h1 =document.createElement('h1');
+    var text=document.createTextNode('your are'+ day +'days older');
+    h1.setAttribute('id','ageInDays');
+    h1.appendChild(text);
+    document.getElementById('hi').appendChild(h1);
+  
+    console.log(day);
+    
+   
+}
+
+function age(){   
+    document.getElementById('ageInDays').remove();
+}
