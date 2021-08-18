81cf5e4b4f01c890836cb190fc8f2f57139909e8
iamsharath15 <iamsharath15@gmail.com>
2021-08-18 23:06:12 +0530

    addstyle

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function ageInDays(){
-    var bob =prompt("dob?");
+    var bob =prompt("enter date of birth year");
     var day = (2021 - bob) * 365;
     var h1 =document.createElement('h1');
     var text=document.createTextNode('your are'+ day +'days older');
